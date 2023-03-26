64eb4906a342929064fef345d5d7ce8c4397c14c
rzl20 <60806163+rzl20@users.noreply.github.com>
2023-03-26 15:37:35 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ let doi;
 if (url.searchParams.get('in') != null) {
   doi = url.searchParams.get('in');
 } else {
-  doi = "Asrii";
+  doi = "Irna";
 }
 
 let footer = document.getElementById("credit");
 footer.innerHTML = doi;
-footer.href = "https://www.instagram.com/zal.zip/";
+footer.href = "https://www.instagram.com/adamukti/";
 
 function time() {
   var d = new Date();
@@ -59,73 +59,71 @@ function selesai() {
 
 document.querySelector(".tombol").addEventListener('click', function () {
   Swal.fire({
-    title: 'Halooo Asriiiiii!',
-    html: `Sebelum masuk ke pointnya ada yg ingin aku sampaikan dulu nih`,
-    timer: 7000,
+    title: 'Hai byy!',
+    html: `Selamat ${time()}, Kamu udah ${makan()}?`,
+    timer: 2500,
     timerProgressBar: true,
     showConfirmButton: false,
   }).then((result) => {
     if (result.dismiss === Swal.DismissReason.timer) {
-      Swal.fire('Pertama, makasih yaa karena kamu udah pakai sepatu yg aku kasih wkwk').then((result) => {
+      Swal.fire('Kalau belum jangan lupa makan yaa').then((result) => {
         Swal.fire({
           title: ' ',
-          html: `Kirain gegara longgar kmu ga bakal pake, eh kmrn pas kegiatan pameran ternyata kamu pakai`,
-          timer: 7000,
+          html: `Aku tau ko kamu lagi sibuk banget`,
+          timer: 2000,
           timerProgressBar: true,
           showConfirmButton: false,
         }).then((result) => {
           if (result.dismiss === Swal.DismissReason.timer) {
             Swal.fire({
               title: ' ',
-              html: `Senang banget aku pas liat, makasih yaa`,
-              timer: 7000,
+              html: `Lagi pusing banget, cape banget`,
+              timer: 2000,
               timerProgressBar: true,
               showConfirmButton: false,
             }).then((result) => {
               if (result.dismiss === Swal.DismissReason.timer) {
-                Swal.fire('oke sekarang kita masuk ke pointnya yaaa').then((result) => {
+                Swal.fire('Semangat terus ya kamu :)').then((result) => {
                   Swal.fire(
-                    'udah sebulan sejak aku menjauh dari kamu, ntah knp sampai sekarang aku blm bisa membiasakan diri dengan keadaan kita yang baru (Alhamdulillah kalo kamu udah)',
+                    'Jangan keseringan begadang!',
                     '',
                     'error'
                   ).then((result) => {
                     Swal.fire(
-                      'aku ngerasa saat kita bertemu kek canggung banget, padahal sblmnya kita enjoyable dengan suasana',
+                      'Jangan telat makan!',
                       '',
                       'error'
                     ).then((result) => {
-                      Swal.fire('trus kamu yang tiap butuh apa apa selalu larinya ke aku, skrng udah gak lagi
-bahkan aku yang tipe orangnya susah cemburu, jujur banget kali ini aku ngerasa cemburu').then((result) => {
+                      Swal.fire('Aku dukung kamu terus ko').then((result) => {
                         Swal.fire({
-                          title: '',
-                          text: 'saat liat kmu yang lagi butuh bantuan tapi mintanya ke orang lain padahal aku ada disitu, perasaan aku kek ga nyaman liat itu
-(jadi kadang kalo udah kek gini aku ngilang)',
+                          title: 'Oh iya aku mau nanya',
+                          text: 'Ada sesuatu yang lagi kamu pingin ga?',
                           showDenyButton: true,
-          
-                          denyButtonText: `lanjut`,
+                          confirmButtonText: `Ada by`,
+                          denyButtonText: `Ga ada ko`,
                         }).then((result) => {
                           /* Read more about isConfirmed, isDenied below */
                           if (result.isConfirmed) {
                             Swal.fire({
-                              title: 'apa tuh?',
+                              title: 'Mau apa?',
                               input: 'text',
-                              inputPlaceholder: 'apa ajaaaa terserah kamuuuu',
+                              inputPlaceholder: 'ex: Seblacc, boba, or etc',
                               showCancelButton: false,
                               inputValidator: (value) => {
                                 if (!value) {
-                                  return 'Isi dulu dong'
+                                  return 'Isi dulu dong by'
                                 }
                               }
                             }).then((result) => {
-                              Swal.fire('Okeeee', 'okeee ntar aku coba lakuin itu', 'success').then((result) => {
-                                Swal.fire("See ya! Semangatttt ^^").then((result) => {
+                              Swal.fire('Okey by', 'Nanti aku kabulin kemauan kamu yaa', 'success').then((result) => {
+                                Swal.fire("See ya! Aku kangen ^^").then((result) => {
                                   selesai()
                                 });
                               })
                             })
                           } else if (result.isDenied) {
-                            Swal.fire('kamu ga salah sih untuk ini, yaa mungkin ini gegara sebelumnya selalu apa apa sama aku. makanya aku cemburu untuk ini').then((result) => {
-                              Swal.fire("jadi yang aku mau tanya ke kamu, apakah caraku kemarin itu salah? cara aku putusin komunikasi kita berdua biar kamu ga terus terusan ngerasa bersalah sama hubungan kemarin").then((result) => {
+                            Swal.fire('Oh okedeh kalo ga ada').then((result) => {
+                              Swal.fire("See ya! Aku kangen ^^").then((result) => {
                                 selesai()
                               });
                             })
